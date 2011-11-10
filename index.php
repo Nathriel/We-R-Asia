b0fb3153e9bd0dd167dccade01fa2cf5421e0c0d
Teun Beijers <teuneboon@gmail.com>
2011-11-10 20:58:47 +0100

    -Did some initial stuff -Added Twig, a template engine -Made an autoloader

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+//this should only call the system class, since we want to program Object-Oriented, just like Java :)
+require_once('./system/System.inc');
+$system = new System();
+$system->run();
+?>
