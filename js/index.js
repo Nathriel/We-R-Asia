0f5ac04a80be0c3daa084cd9355bd1f3e0bd6e1b
Nathriel <markiebosie@hotmail.com>
2012-01-05 15:12:39 +0100

    added a custom dialog box (see login)

--- js/index.js
+++ js/index.js
@@ -1,4 +1,26 @@
 $(function() {
+	
+	/* menu */
+	$('#menu, #drop_menu').hover(function() {
+		$('#drop_menu').show();
+	},function(){
+		$('#drop_menu').hide();
+	});
+	
+	/* dialog_box */
+	$('#dialog_box a').click(function() {
+		$('#dialog_box').hide();
+		$('#dialog_bg').hide();
+		$('#dialog_box p').empty();
+	});
+	
+	function showDialog(text) {
+		$('#dialog_box p').prepend(text);
+		$('#dialog_bg').show();
+		$('#dialog_box').show();
+	}
+	
+	/* login */
 	$('#loginForm').submit(function(e) {
 		e.preventDefault();
 		var username = $('#loginForm input[name="username"]').val();
@@ -13,13 +35,12 @@ $(function() {
 			'success': function(data) {
 				if (data == 1) {
 					// successful login!
-					alert('successful login!');
                     //window.location='';
                     var url = window.location;
                     window.location = url + 'projects'
 				} else {
 					// you dumbass used the wrong username or password!
-					alert('you dumbass used the wrong username or password!');
+					showDialog('you dumbass used the wrong username or password!');
 				}
 			}
 		});
--- js/menu.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-	$('#menu, #drop_menu').hover(function(){
-		$('#drop_menu').show();
-	},function(){
-		$('#drop_menu').hide();
-	});
-});
